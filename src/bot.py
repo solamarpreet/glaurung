8772a92ccc99462d44be37452d86d1af2fe1bbba
Amarpreet Singh <solamarpreet@gmail.com>
2023-04-19 19:53:16 +0530

    Added db and personality handling

--- src/bot.py
+++ src/bot.py
@@ -2,23 +2,24 @@ import discord
 import openai_integration
 import config
 
-def run():
-    
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
 
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
 
-        async with message.channel.typing():
-            completion = openai_integration.get_chat_completion(message)
-            await message.channel.send(completion)
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-    client.run(config.get_discord_token())
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+
+    async with msg.channel.typing():
+        completion = openai_integration.completion_handler(msg)
+        await msg.channel.send(completion)
+
+
+client.run(config.get_discord_token())
--- src/openai_integration.py
+++ src/openai_integration.py
@@ -2,20 +2,41 @@ import openai
 import config
 
 openai.api_key = config.get_openai_token()
+db = {}
 
-def get_chat_completion(message):
-    list_handler(message)
+
+def completion_handler(msg):
+    print(msg.content)
+    if msg.content.startswith("glau"):
+        if msg.content.startswith("glau reset"):
+            db[msg.author.id] = [
+            {"role": "system", "content": "You are a helpful tutor that explains programming, devops concepts. You also generate code when asked, giving examples."}
+            ]
+            print(db)
+            return "Noted !"
+        else:
+            db[msg.author.id] = [
+            {"role": "system", "content": msg.content[5:]}
+            ]
+            return get_chat_completion(msg)
+
+    elif db.get(msg.author.id):
+        db[msg.author.id].append({"role": "user", "content": msg.content})
+
+    else:
+        db[msg.author.id] = [
+            {"role": "system", "content": "You are a helpful tutor that explains programming, devops concepts. You also generate code when asked, giving examples."},
+            {"role": "user", "content": msg.content}
+            ]
+        
+    return get_chat_completion(msg)
+
+
+def get_chat_completion(msg):    
+    print(db)
     completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=message_db[message.author]
+            messages=db[msg.author.id]
             )
-    message_db[message.author].append({"role": "assistant", "content": completion.choices[0].message.content})
+    db[msg.author.id].append({"role": "assistant", "content": completion.choices[0].message.content})
     return completion.choices[0].message.content
-
-def list_handler(message):
-    if message_db.message.author:
-        message_db[message.author].append({"role": "user", "content": message.content})
-    else:
-        message_db[message.author] = [{"role": "system", "content": "You are a helpful tutor that explains programming, devops concepts. You also generate code when asked, giving examples."}]
-
-message_db = dict()
